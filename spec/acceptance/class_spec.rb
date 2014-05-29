6c4ffbbc731bf065a5ac16f6721c38bd0aa1de03
Peter Souter <p.morsou@gmail.com>
2014-05-29 17:05:04 +0100

    Added spec for specific swapfile

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -13,6 +13,9 @@ describe 'swap_file', :unless => UNSUPPORTED_PLATFORMS.include?(fact('osfamily')
         expect(apply_manifest(pp).exit_code).to_not eq(1)
         expect(apply_manifest(pp).exit_code).to eq(0)
       end
+      it 'should contain the default swapfile' do
+        shell('/sbin/swapon -s | grep /mnt/swap.1', :acceptable_exit_codes => [0])
+      end
     end
   end
 end
