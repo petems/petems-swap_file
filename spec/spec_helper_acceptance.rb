b763850d302d72c7e684c8bb6f81c89d13646494
Peter Souter <peter.souter@puppetlabs.com>
2015-07-27 13:39:55 +0100

    Remove hiera creation (now done by Beaker auto)

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -25,7 +25,6 @@ RSpec.configure do |c|
     # Install module and dependencies
     puppet_module_install(:source => proj_root, :module_name => 'swap_file')
     hosts.each do |host|
-      shell('/bin/touch /etc/puppet/hiera.yaml')
       shell('puppet module install puppetlabs-stdlib --version 3.2.0', { :acceptable_exit_codes => [0,1] })
     end
   end
